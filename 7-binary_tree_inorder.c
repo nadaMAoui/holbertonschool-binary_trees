f235cfc0fb67210bb38b6caede746945d75dc174
nadaMAoui <maaouinada747@gmail.com>
2023-01-11 13:55:31 +0100

    traverse a BT using inorder traverse

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_inorder - visit nodes using ineorder traverse
+ * @tree: pointer to the root node
+ * @func: pointer to the func to call
+ * Return: void
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	func(tree->n);
+	if (tree->left != NULL)
+		binary_tree_inorder(tree->left, func);
+	if (tree->right != NULL)
+		binary_tree_inorder(tree->right, func);
+}
