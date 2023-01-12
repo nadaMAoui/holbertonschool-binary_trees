0dede89c500a5cf70d70583c04072a8037b1c236
nadaMAoui <maaouinada747@gmail.com>
2023-01-12 11:26:23 +0100

    countes number of nodes holding at least 1 child

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_nodes - counts nodes with at
+ *least 1child
+ *@tree: pointer to the root
+ *Return: number of nodes holding at leat1 child
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	if ((tree == NULL) || (!tree->left && !tree->right))
+	{
+		return (0);
+	}
+
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
+	return (nodes + 1);
+}
