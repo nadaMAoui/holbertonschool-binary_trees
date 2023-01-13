5f484dfcff9e7c8417d0e7fae6d296824dda67a8
nadaMAoui <maaouinada747@gmail.com>
2023-01-13 22:29:43 +0100

    perfect BT checking ... case root 98

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -48,13 +48,13 @@ int sub_tree_perfect(const binary_tree_t *tree)
 /**
  *binary_tree_is_perfect - checks if binary tree is perfect
  *@tree: pointer to the root
- *Return: 0 fail or 1 successes
+ *Return: 0 fail or 1 successing 
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
 	if (binary_tree_balance(tree) != 0)
-		return (1);
+		return (0);
 	return (sub_tree_perfect(tree->left) && sub_tree_perfect(tree->right));
 }
