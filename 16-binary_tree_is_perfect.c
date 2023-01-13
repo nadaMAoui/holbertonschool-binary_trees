906e32ed03e9ead7ae717304b1efb54a7be61585
nadaMAoui <maaouinada747@gmail.com>
2023-01-13 22:13:09 +0100

    perfect BT....

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,7 +8,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
-	if (tree == NULL)
+	if (!tree)
 	{
 		return (0);
 	}
@@ -27,7 +27,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
